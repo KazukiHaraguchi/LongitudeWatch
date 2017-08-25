6e6bc8c12deccd23735810e1581206283eb526da
TAITSUBASA <tsuba283the-dolphins.power1@docomo.ne.jp>
2017-08-25 16:11:25 +0900

    clock test

--- LongitudeWatch/src/com/example/longitudewatch/Clock.java
+++ LongitudeWatch/src/com/example/longitudewatch/Clock.java
@@ -1,5 +1,4 @@
 package com.example.longitudewatch;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,9 +7,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.location.*;
+import android.content.*;
+import android.util.Log;
+import android.widget.TextView;
 
-public class Clock extends Activity {
-
+public class Clock extends Activity implements LocationListener{
+	LocationManager locmanG;
+	LocationManager locmanN;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,6 +41,8 @@ public class Clock extends Activity {
 				startActivity(intent);
 			}
 		});
+		locmanG =(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		locmanN =(LocationManager)getSystemService(Context.LOCATION_SERVICE);
 	}
 
 	@Override
@@ -56,4 +63,67 @@ public class Clock extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	@Override
+	protected void onResume() {
+
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		if (locmanG != null) {
+			locmanG.removeUpdates(this);
+		}
+		if (locmanN != null) {
+			locmanN.removeUpdates(this);
+		}
+		super.onPause();
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		if (location.getProvider().equals(LocationManager.GPS_PROVIDER) == true) {
+			TextView textView3 = (TextView) findViewById(R.id.ClockText);
+			textView3.setText("GPS Latitude:Longitude - "
+					+ String.valueOf(location.getLatitude()) + ":"
+					+ String.valueOf(location.getLongitude()));
+
+
+
+			Log.v("----------", "----------");
+			Log.v("Latitude", String.valueOf(location.getLatitude()));
+			Log.v("Longitude", String.valueOf(location.getLongitude()));
+			Log.v("Accuracy", String.valueOf(location.getAccuracy()));
+			Log.v("Altitude", String.valueOf(location.getAltitude()));
+			Log.v("Time", String.valueOf(location.getTime()));
+			Log.v("Speed", String.valueOf(location.getSpeed()));
+			Log.v("Bearing", String.valueOf(location.getBearing()));
+		}
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		switch (status) {
+		case LocationProvider.AVAILABLE:
+			Log.v("Status", "AVAILABLE");
+			break;
+		case LocationProvider.OUT_OF_SERVICE:
+			Log.v("Status", "OUT_OF_SERVICE");
+			break;
+		case LocationProvider.TEMPORARILY_UNAVAILABLE:
+			Log.v("Status", "TEMPORARILY_UNAVAILABLE");
+			break;
+		}
+	}
 }
+
+
